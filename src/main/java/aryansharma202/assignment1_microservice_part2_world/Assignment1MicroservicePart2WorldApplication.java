3ec280184d99f32383813097eb0e6217354e582b
Aryan8Sharma <arisharma01@gmail.com>
2024-09-04 13:48:04 -0700

    first commit

---
+++ src/main/java/aryansharma202/assignment1_microservice_part2_world/Assignment1MicroservicePart2WorldApplication.java
@@ -0,0 +1,13 @@
+package aryansharma202.assignment1_microservice_part2_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1MicroservicePart2WorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assignment1MicroservicePart2WorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aryansharma202/assignment1_microservice_part2_world/Assignment1MicroservicePart2WorldApplicationTests.java
@@ -0,0 +1,13 @@
+package aryansharma202.assignment1_microservice_part2_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1MicroservicePart2WorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
